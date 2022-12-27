f0929165f1b96263501f0297fcfb8c306885f958
Marcelo Pastana Duarte <marcelopd20@gmail.com>
2022-12-27 12:54:51 -0300

    Readme Updated

--- src/api/yahoo-finance-api.test.ts
+++ src/api/yahoo-finance-api.test.ts
@@ -4,33 +4,26 @@ import { yahooAssetDto } from '~/tests/mocks/yahoo-asset-dto-mocks'
 import YahooFinanceApi, { YahooQuoteResponse } from './yahoo-finance-api'
 import HttpException from '~/exceptions/http-exception'
 
-// conecta com api
 const yahooFinanceApi = new YahooFinanceApi()
-// passa symbol para test
+
 const symbol: string = 'TEST'
-// testa getAsset
+
 describe('getAsset()', () => {
-  // testa sucesso
   test('Sucess', async () => {
     // arrange
-    // passa resultado esperado
     const expectedAsset: YahooAssetDto = yahooAssetDto
-    // passa json esperado
     const expectedYahooResponse: YahooQuoteResponse = {
       quoteResponse: {
         result: [expectedAsset]
       }
     }
-    // mocka api com nock, /quote?symbols=symbol espera status 200 e resposta
     nock('https://yfapi.net/v6/finance')
       .get('/quote')
       .query({ symbols: symbol })
       .reply(200, expectedYahooResponse)
     // act
-    // testa mmétodo getAsset passando string
     const response: YahooAssetDto = await yahooFinanceApi.getAsset(symbol)
     // assert
-    // verifica chave/valor aguardado
     expect(response.currency).toStrictEqual(expectedAsset.currency)
     expect(response.market).toStrictEqual(expectedAsset.market)
     expect(response.regularMarketPrice).toStrictEqual(expectedAsset.regularMarketPrice)
@@ -38,7 +31,7 @@ describe('getAsset()', () => {
     expect(response.symbol).toStrictEqual(expectedAsset.symbol)
     expect(response.typeDisp).toStrictEqual(expectedAsset.typeDisp)
   })
-  // simula retorno vazio
+
   test('Yahoo finance api returning an empty result', async () => {
     // arrange
     const expectedYahooResponse: YahooQuoteResponse = {
@@ -46,16 +39,14 @@ describe('getAsset()', () => {
         result: []
       }
     }
-    // mocka api com nock, /quote?symbols=symbol espera status 200 e resposta
+    
     nock('https://yfapi.net/v6/finance')
       .get('/quote')
       .query({ symbols: symbol })
       .reply(200, expectedYahooResponse)
     // act
-    // verifica resposta
     const responseCb = () => yahooFinanceApi.getAsset(symbol)
     // assert
-    // valida exceção esperada pelo método
     await expect(responseCb).rejects.toThrowError(HttpException)
   })
 })
--- src/api/yahoo-finance-api.ts
+++ src/api/yahoo-finance-api.ts
@@ -3,18 +3,15 @@ import { Environments } from '~/config/environment'
 import YahooAssetDto from '~/dtos/yahoo-asset-dto'
 import HttpException from '~/exceptions/http-exception'
 
-// tipo de resposta que recebe
 export type YahooQuoteResponse = {
   quoteResponse: {
     result: YahooAssetDto[]
   }
 }
 
-// classe do YahooFinanceApi
 export default class YahooFinanceApi {
   private readonly client: AxiosInstance
 
-  // conecta com api pelo axios
   public constructor() {
     this.client = axios.create({
       baseURL: 'https://yfapi.net/v6/finance',
@@ -24,22 +21,17 @@ export default class YahooFinanceApi {
     })
   }
 
-  // método de busca na API
   public async getAsset(symbol: string): Promise<YahooAssetDto> {
     try {
-      // faz get na conexao client, passando symbol para parametro
       const response: AxiosResponse = await this.client.get('/quote', {
         params: {
           symbols: symbol
         }
       })
-      // pega primeiro item da lista result
       const asset: YahooAssetDto = (response.data as YahooQuoteResponse)?.quoteResponse?.result[0]
-      // em caso de return com lista vazia, false, passa para erro
       if (!asset) {
         throw new HttpException(404, `Asset '${symbol}' not found!`)
       }
-      // retorna dados normalizados encntrados
       return {
         currency: asset?.currency,
         market: asset?.market,
@@ -49,15 +41,12 @@ export default class YahooFinanceApi {
         typeDisp: asset?.typeDisp
       }
     } catch (error: unknown) {
-      // passa exceção ou erro interno, se problema com client
       if (error instanceof AxiosError) {
         throw new HttpException(error.response?.status ?? 500, error.message)
       }
-      // passa erro
       if (error instanceof HttpException) {
         throw error
       }
-      // se não houver lista, symbol é recusado
       throw new HttpException(500, `Fail to fetch asset ${symbol} on Yahoo Finance API`)
     }
   }
