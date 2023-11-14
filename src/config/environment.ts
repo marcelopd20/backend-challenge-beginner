ce694fc2d1acde4e6b359ac22036ce690b350b4d
Marcelo Pastana Duarte <86241663+marcelopd20@users.noreply.github.com>
2023-11-14 16:22:26 -0300

    Update environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const Environments = {
-  API_BASE_PATH: process.env.API_BASE_PATH ?? '/assets-watch-api/v1',
+  API_BASE_PATH: process.env.API_BASE_PATH ?? '',
   SERVER_PORT: Number(process.env.SERVER_PORT ?? 3000),
   YAHOO_FINANCE_API_KEY: process.env.YAHOO_FINANCE_API_KEY,
   DB_CONNECTION_STRING: String(process.env.DB_CONNECTION_STRING),
-  DB_ASSET_NAME: process.env.DB_ASSET_NAME ?? 'assets'
+  DB_ASSET_NAME: process.env.DB_ASSET_NAME ?? ''
 }
